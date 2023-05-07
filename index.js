10ea7903eb728727ec838cfa60d9633a20d48ed9
victorsolaris <victorsolaris095@gmail.com>
2023-05-07 15:06:50 +0000

    js objects

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-    // New JavaScript code
-    console.log('Hello World');
-    
-    const interestRate = 0.3;
-    interestRate = 1;
-    console.log(interestRate);
+let person = {
+    name: 'Mosh',
+    age: 30
+} // object literal
 
-    let name = 'Mosh'; // string literal
-    let age = 30; // number literal
-    let isApproved = true; // boolean literal, true/false are reserved keywords
-    let firstName = undefined; // not common
-    let lastName = null; // explicilty clear the value of a variable, ex. no selection of a value
+console.log(person);
