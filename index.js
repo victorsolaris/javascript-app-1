a752371baad989f58407f6f2c320d3d892e15c89
victorsolaris <victorsolaris095@gmail.com>
2023-05-07 15:11:42 +0000

    bracket notation, variable changes at runtime

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ person.name = 'John';
 
 // Bracket notation, single or double quots
 person['name'] = 'Mary';
+
+// Bracket notation, variable changes at runtime
+let selection = 'name';
+person[selection] = 'Mary';
+
 console.log(person);
