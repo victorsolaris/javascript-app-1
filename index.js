915f8ac01da65eec3ec6c47705840bd9cc14d8ba
victorsolaris <victorsolaris095@gmail.com>
2023-05-07 15:53:11 +0000

    dynamic objects

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // list of objects in an array
 
 let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+selectedColors[3] = 1;
 console.log(selectedColors);
 console.log(selectedColors[0]);
