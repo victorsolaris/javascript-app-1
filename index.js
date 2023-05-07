07860909a8ebb00df38e3b927598de20aeff41d7
victorsolaris <victorsolaris095@gmail.com>
2023-05-07 16:12:41 +0000

    function parameters function

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // function
 
-function greet(name) {
-    console.log('Hello ' + name);
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
 }
 
-greet('John');
-greet('Mary');
+
+// Calculating a value
+
+function square(number) {
+    return number * number;
+}
+let number = square(2)
+console.log(number);
