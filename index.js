f5f621c23302ef9804a721c62e469f52a352677d
victorsolaris <victorsolaris095@gmail.com>
2023-05-07 14:54:47 +0000

    variables

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
     // New JavaScript code
     console.log('Hello World');
+    
+    const interestRate = 0.3;
+    interestRate = 1;
+    console.log(interestRate);
+
+    let name = 'Mosh'; // string literal
+    let age = 30; // number literal
+    let isApproved = true; // boolean literal, true/false are reserved keywords
+    let firstName = undefined; // not common
+    let lastName = null; // explicilty clear the value of a variable, ex. no selection of a value
